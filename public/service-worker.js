386ecc95b8108efb9fe4188958301c2fd268b5c6
Rich Capuy <rccapuy@gmail.com>
2020-08-08 20:09:54 -0400

    spelling error

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,8 +6,8 @@ const FILES_TO_CACHE = [
 	'/index.js',
 	'/manifest.webmanifest',
 	'/styles.css',
-	'/icons/icons-192x192.png',
-	'/icons/icons-512x512.png'
+	'/icons/icon-192x192.png',
+	'/icons/icon-512x512.png'
 ];
 
 const CACHE_NAME = "static-cache-v2";
